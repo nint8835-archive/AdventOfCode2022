1de05210b4410b65f36b7060a9f196520d408db2
Riley Flynn <riley@rileyflynn.me>
2022-12-01 07:04:48 -0330

    Day 1

---
+++ Day1/Day1.fsx
@@ -0,0 +1,17 @@
+let inputData =
+    (System.IO.File.ReadAllText "Day1/input.txt")
+        .Split "\n\n"
+        |> Array.map
+               (fun elf ->
+                    elf.Split("\n")
+                    |> Array.map int)
+
+let maxNElves (elves: int[][]) (n: int) : int =
+    elves
+    |> Array.map Array.sum
+    |> Array.sortDescending
+    |> (fun (array: int[]) -> array[..n - 1])
+    |> Array.sum
+
+printfn $"Part A: {maxNElves inputData 1}"
+printfn $"Part B: {maxNElves inputData 3}"
