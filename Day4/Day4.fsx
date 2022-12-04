03fc4853af819604590b0823e743f89af9d37759
Riley Flynn <riley@rileyflynn.me>
2022-12-04 12:21:46 -0330

    Day 4, cleanup pass

--- Day4/Day4.fsx
+++ Day4/Day4.fsx
@@ -5,23 +5,22 @@ let inputData =
         line.Split ","
         |> Array.map (fun elf -> elf.Split "-" |> Array.map int))
 
-let partA (pair: int [] []) : bool =
-    ((pair[0][0] <= pair[1][0])
-     && (pair[0][1] >= pair[1][1]))
-    || ((pair[1][0] <= pair[0][0])
-        && (pair[1][1] >= pair[0][1]))
+let tryBothOrders (func: int [] -> int [] -> bool) (pair: int [] []) : bool =
+    func pair[0] pair[1] || func pair[1] pair[0]
 
-let partB (pair: int [] []) : bool =
-    (pair[0][0] <= pair[1][0]
-     && pair[1][0] <= pair[0][1])
-    || (pair[1][0] <= pair[0][0]
-        && pair[0][0] <= pair[1][1])
+let partA (a: int []) (b: int []) : bool = a[0] <= b[0] && a[1] >= b[1]
+
+let partB (a: int []) (b: int []) : bool = a[0] <= b[0] && b[0] <= a[1]
 
 let partAResult =
-    inputData |> Array.filter partA |> Array.length
+    inputData
+    |> Array.filter (tryBothOrders partA)
+    |> Array.length
 
 let partBResult =
-    inputData |> Array.filter partB |> Array.length
+    inputData
+    |> Array.filter (tryBothOrders partB)
+    |> Array.length
 
 printfn $"%A{partAResult}"
 printfn $"%A{partBResult}"
