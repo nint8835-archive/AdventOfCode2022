81cacc5ac3f0048943c6168af4272e04d96c4dcf
Riley Flynn <riley@rileyflynn.me>
2022-12-06 06:58:20 -0330

    Handle only one line of input, use operator as function

--- Day6/Day6.fsx
+++ Day6/Day6.fsx
@@ -1,16 +1,13 @@
 let inputData =
     (System.IO.File.ReadAllText "Day6/input.txt")
-        .Split "\n"
 
-let findMarker (size: int) (input: string []) : int [] =
+let findMarker (size: int) (input: string) : int =
     input
-    |> Array.map (fun line ->
-        line
-        |> Array.ofSeq
-        |> Array.windowed size
-        |> Array.map (fun window -> (window |> Set.ofArray |> Set.count))
-        |> Array.findIndex (fun uniqueCount -> uniqueCount = size)
-        |> (fun index -> index + size))
+    |> Array.ofSeq
+    |> Array.windowed size
+    |> Array.map (fun window -> (window |> Set.ofArray |> Set.count))
+    |> Array.findIndex (fun uniqueCount -> uniqueCount = size)
+    |> (+) size
 
 printfn $"%A{findMarker 4 inputData}"
 printfn $"%A{findMarker 14 inputData}"
