be72f818f75534ebe87ef8e28759e8fbd5756f51
Riley Flynn <riley@rileyflynn.me>
2022-12-08 19:58:01 -0330

    Day 8, part 2

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-
-
 def get_visible(grid: list[list[int]], direction: str) -> set[tuple[int, int]]:
     height = len(grid)
     width = len(grid[0])
@@ -25,11 +22,56 @@ def get_visible(grid: list[list[int]], direction: str) -> set[tuple[int, int]]:
                 visible.add(coord_getter(line_index, i))
             val = line[i]
             if val > max:
-                print(val, coord_getter(line_index, i))
                 max = val
                 visible.add(coord_getter(line_index, i))
 
     return visible
+
+
+def get_scenic_distance(
+    grid: list[list[int]], coordinate: tuple[int, int], direction: str
+) -> int:
+    heights = []
+    if direction == "up":
+        for row_index, row in enumerate(grid):
+            if row_index >= coordinate[1]:
+                continue
+
+            heights.append(row[coordinate[0]])
+        heights.reverse()
+    elif direction == "down":
+        for row_index, row in enumerate(grid):
+            if row_index <= coordinate[1]:
+                continue
+
+            heights.append(row[coordinate[0]])
+    elif direction == "right":
+        row = grid[coordinate[1]]
+        for col_index, col in enumerate(row):
+            if col_index <= coordinate[0]:
+                continue
+
+            heights.append(col)
+    elif direction == "left":
+        row = grid[coordinate[1]]
+        for col_index, col in enumerate(row):
+            if col_index >= coordinate[0]:
+                continue
+
+            heights.append(col)
+
+        heights.reverse()
+
+    val = grid[coordinate[1]][coordinate[0]]
+
+    if len(heights) == 0:
+        return 0
+
+    for distance, height in enumerate(heights):
+        if height >= val:
+            return distance + 1
+
+    return len(heights)
 
 
 with open("input.txt") as f:
@@ -38,4 +80,15 @@ with open("input.txt") as f:
 visible_sets = [get_visible(grid, direction) for direction in ["lr", "rl", "td", "bu"]]
 all_visible = set.union(*visible_sets)
 print(len(all_visible))
-# pprint(all_visible)
+
+scenic_distances = []
+for y in range(len(grid)):
+    for x in range(len(grid[0])):
+        scenic_distances.append(
+            get_scenic_distance(grid, (x, y), "up")
+            * get_scenic_distance(grid, (x, y), "down")
+            * get_scenic_distance(grid, (x, y), "left")
+            * get_scenic_distance(grid, (x, y), "right")
+        )
+
+print(sorted(scenic_distances, reverse=True)[0])
